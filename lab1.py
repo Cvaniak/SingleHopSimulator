7048e669b104941fc8584506bef9d8e4b7bf74d7
Cvaniak <igna.cwaniak@gmail.com>
2021-03-16 23:29:11 +0100

    Final Version V1.0, Wednesday

--- lab1.py
+++ lab1.py
@@ -4,8 +4,9 @@ import pandas as pd
 import random
 import numpy as np
 from collections import defaultdict
-from math import log, e
+from math import log, e, prod, sqrt
 #  -------------------------------------------------------------------------
+# Matplot style taken from:
 # https://towardsdatascience.com/cyberpunk-style-with-matplotlib-f47404c9d4c5
 plt.style.use("dark_background")
 for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
@@ -21,34 +22,9 @@ colors = [
 n_shades = 10
 diff_linewidth = 0.6/10
 alpha_value = 0.6 / n_shades
-HISTO_COUNTER = 1
-# df = pd.DataFrame({'A': [1, 3, 9, 5, 2, 1, 1],
-#                    'B': [4, 5, 5, 7, 9, 8, 6]})
-# fig, ax = plt.subplots()
-# df.plot(marker='o', color=colors, ax=ax)
-# Redraw the data with low alpha and slighty increased linewidth:
-
-# for n in range(1, n_shades+1):
-#     df.plot(marker='o',
-#             linewidth=2+(diff_linewidth*n),
-#             alpha=alpha_value,
-#             legend=False,
-#             ax=ax,
-#             color=colors)
-# # Color the areas below the lines:
-# for column, color in zip(df, colors):
-#     ax.fill_between(x=df.index,
-#                     y1=df[column].values,
-#                     y2=[0] * len(df),
-#                     color=color,
-#                     alpha=0.1)
-# ax.grid(color='#2A3459')
-# ax.set_xlim([ax.get_xlim()[0] - 0.2, ax.get_xlim()[1] + 0.2])  # to not have the markers cut off
-# ax.set_ylim(0)
-# plt.show()
 #  -------------------------------------------------------------------------
 
-def draw_bar(fig, ax, a, b):
+def draw_bar(a, b):
     plt.bar(a, b, color=colors, width=0.2)
     for i in range(n_shades):
         plt.bar(a, b,
@@ -57,104 +33,115 @@ def draw_bar(fig, ax, a, b):
                         alpha=alpha_value
                         )
     plt.xticks(a, a)
-    # xpoints = np.array([0, 6])
-    # ypoints = np.array(range())
-    # ax.set_xlim([ax.get_xlim()[0] - 0.2, ax.get_xlim()[1] + 0.2])  # to not have the markers cut off
-    # ax.set_ylim(0)
 
 
 class Node():
-
     def __init__(self, index, nodes_number):
         self.index = index
         self.nodes_number = nodes_number
         self.last_vote = -1
 
-    def vote(self):
+    def vote(self, number):
+        pass
+
+
+class Node_scenario_2(Node):
+    def __init__(self, index, nodes_number):
+        super().__init__(index, nodes_number)
+
+    def vote(self, number):
         self.last_vote = self.index if random.randint(1, self.nodes_number+1) == self.index else 0
         return self.last_vote
 
 
-class NodeSc2(Node):
+class Node_scenario_3(Node):
     def __init__(self, index, nodes_number):
         super().__init__(index, nodes_number)
-        self.L = log(nodes_number, 2)
-        self.round = 1
+        self.L = int(log(nodes_number, 2))
     
-    def vote(self):
-        if self.round > self.L:
-            self.round = 1
-        
-        self.last_vote = self.index if random.randint(1, 2**self.round) == self.round//2 else 0
+    def vote(self, number):
+        self.last_vote = self.index if random.randint(1, 2**(number%self.L + 1)) == 1 else 0
         return self.last_vote
 
-def create_nodes(nodes_number):
-    return [Node(index, nodes_number) for index in range(1, nodes_number+1)]
-    
-def create_nodes_sc2(nodes_number, u_number):
-    return [Node(index, u_number) for index in range(1, nodes_number+1)]
 
-def main(loop_counter, nodes, nodes_number=100, is_plt=0):
-    global HISTO_COUNTER
+def create_nodes_scenario_2(nodes_number):
+    return [Node_scenario_2(index, nodes_number) for index in range(1, nodes_number+1)]
+
+def create_nodes_scenario_3(nodes_number, u_number):
+    return [Node_scenario_3(index, u_number) for index in range(1, nodes_number+1)]
+
+
+def exercise_2():
+    loop_counter = 1000
+    u_v = 1024
+
+    nodes = create_nodes_scenario_2(u_v) 
+    symulation(loop_counter, nodes, 1)
+    nodes = create_nodes_scenario_3(u_v, u_v) 
+    symulation(loop_counter, nodes[:2], 1)
+    symulation(loop_counter, nodes[:u_v//2], 1)
+    symulation(loop_counter, nodes, 1)
+
+    plt.show() 
+
+def exercise_3():
+    loop_counter = 1000
+    n = 1024
+    nodes =  create_nodes_scenario_2(n)
+    symulation(loop_counter, nodes, ex_3=1)
+
+def exercise_4():
+    min1 = 2
+    max1 = -1
+    nodes = create_nodes_scenario_3(1024, 1024) 
+    for i in range(2,1024, 256):
+        r = symulation(1000, nodes[:i], 0, ex_4=1)
+        min1 = min(r, min1)
+        max1 = max(r, max1)
+    print(min1,max1)
+
+
+def choose_lider(nodes):
     counter = 0
-    last_lider = 0
-    d = defaultdict(int)
-    d1 = defaultdict(int)
-    d2 = []
+    while True:
+        slot = len([node.last_vote for node in nodes if node.vote(counter)])
+        if slot == 1:
+            return counter + 1
+        counter += 1
 
-    while counter < loop_counter:
-        counter += 1 
-        slot = [node.last_vote for node in nodes if node.vote()]
-        if len(slot) == 1:
-            # print(f"{slot[0]} \n", end="")
-            d[last_lider] += 1
-            d1[counter] = last_lider
-            d2.append(last_lider)
-            last_lider = 1
-        else:
-            # print(f"{'|' if len(slot) else '-'}{slot}", end="")
-            d1[counter] = 0
-            last_lider += 1
 
-    elog = []
-    # for i in range(max(d.keys())):
-    #     # elog.append(e*log(i+1))
-    #     if not d[i]:
-    #         d[i] = 0
+def symulation(number_of_experiments, nodes_list, plotting=0, ex_3=0, ex_4=0):
+    print((f"Symulation for {number_of_experiments:5} tries"
+            f", type of node is {type(nodes_list[0]).__name__}"
+            f", with {len(nodes_list):5} nodes, plotting is {plotting}"
+            f", ex_3 is {ex_3} and ex_4 is {ex_4}"
+            ))
+    lider_slot = defaultdict(int)
 
-    if is_plt:
-        d = dict(sorted(d.items(), key=lambda item: item[0]))
-        plt.subplot(int(f'42{HISTO_COUNTER}'))
-        draw_bar(None, None, list(d.keys()), list(d.values()))
-        HISTO_COUNTER += 1
-        plt.subplot(int(f'42{HISTO_COUNTER}'))
-        HISTO_COUNTER += 1
-        plt.bar(range(len(d1)), list(d1.values()), color=colors)
+    for i in range(number_of_experiments):
+       lider_slot[choose_lider(nodes_list)] += 1 
 
-    temp = sum([k*v for k,v in d.items()])
-    print(f"{temp} - {sum(d.values())}")
-    print(temp/sum(d.values()))
+    if plotting:
+        lider_slot = dict(sorted(lider_slot.items(), key=lambda item: item[0]))
+        plt.figure()
+        draw_bar(list(lider_slot.keys()), list(lider_slot.values()))
+        
+    if ex_3:
+        expected = sum([v*k for k,v in lider_slot.items()])/number_of_experiments
+        variance = sqrt(sum([(expected - v*k/number_of_experiments)**2
+                    for k,v in lider_slot.items()])/(number_of_experiments-1))
+        print(f"Expected value: {expected} , Variance value: {variance}")
 
+    if ex_4:
+        g = prod([1-(v)/number_of_experiments for k, v in lider_slot.items()])
+        print(f"{len(nodes_list)} ")
+        lider_slot = dict(sorted(lider_slot.items(), key=lambda item: item[0]))
+        print(1-g)
+        return 1-g 
 
 
 if __name__ == '__main__':
-    # random.seed(1234)
-    loop_counter = 1000
-    # nodes_number = input() or 100
-    nodes = create_nodes(100) 
-    main(int(loop_counter), nodes, 100, 1)
-    nodes = create_nodes_sc2(2, 100) 
-    main(int(loop_counter), nodes, 100, 1)
-    nodes = create_nodes_sc2(50, 100) 
-    main(int(loop_counter), nodes, 100, 1)
-    nodes = create_nodes_sc2(100, 100) 
-    main(int(loop_counter), nodes, 100, 1)
-    plt.show()
-
-    for i in range(0,20, 4):
-        for j in range(i, 20, 4):
-            nodes = create_nodes_sc2(i, j) 
-            main(int(loop_counter), nodes, j)
-
-
+    exercise_2()
+    exercise_3()
+    exercise_4()
 
